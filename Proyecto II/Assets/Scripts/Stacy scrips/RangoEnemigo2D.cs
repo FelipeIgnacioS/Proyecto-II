77df982bff7f1498915100ef1b7863e341c663fd
jugo123 <103468623+jugo123@users.noreply.github.com>
2022-05-11 18:25:13 -0400

    Stacy (enemigo 1 dinosaurio)

---
+++ Proyecto II/Assets/Scripts/Stacy scrips/RangoEnemigo2D.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangoEnemigo2D : MonoBehaviour
+{
+    public Animator ani;
+    public Enemigo2D enemigo;
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.CompareTag("Ucenin"))
+        {
+           ani.SetBool("walk", false);
+           ani.SetBool("run", false);
+           ani.SetBool("attack", true);        
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
